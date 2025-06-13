9f3eb7963d5f6a9c9f744eeb6a0907ffd193f640
raheem7348 <raheem7348@gmail.com>
2025-06-13 17:17:43 +0530

    chnages made

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { checkHeading, replaceHeadingStars } from '../helper'
-
+import ReactMarkdown from 'react-markdown'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { stringify } from 'postcss'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const Answers = ({ ans, index, totalResult, type }) => {
     const [heading, setHeading] = useState(false)
@@ -15,7 +18,24 @@ const Answers = ({ ans, index, totalResult, type }) => {
 
     }, [])
 
-    // console.log(index);
+     const renderer = {
+        code({node, inline, className, children, ...props}) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match?(
+                <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={dark}
+                PreTag='div'
+                />
+            ):(
+            <code {...props} className = {className}>
+                {children}
+            </code>
+            )
+     }
+    }
 
 
     return (
@@ -25,7 +45,11 @@ const Answers = ({ ans, index, totalResult, type }) => {
                 index == 0 && totalResult > 1 ? <span className='text-lg block pt-2'>{answer}</span> :
                     heading ?
                         <span className='text-xl block pt-2'>{answer}</span>
-                        : <span className={type=='q' ? 'pl-1' : 'pl-5'}>{answer}</span>
+                        : <span className={type=='q' ? '' : 'pl-5'}>
+                            <ReactMarkdown components = {renderer}>
+                            {answer}
+                            </ReactMarkdown>
+                            </span>
             }
         </>
     )
--- src/components/QuestionAnswer.jsx
+++ src/components/QuestionAnswer.jsx
@@ -14,7 +14,7 @@ const QuestionAnswer = ({item, index}) => {
                         rounded-tl-3xl rounded-bl-3xl rounded-br-3xl w-fit ">
                             <Answers ans={item.text} index={index} totalResult={1} type={item.type} /></li>
                         : item.text.map((ansItem, ansIndex) => (
-                            <li key={index} className="text-left p-1"><Answers ans={ansItem} index={ansIndex} totalResult={item.length} type={item.type} /></li>
+                            <li key={ansIndex} className="text-left p-1"><Answers ans={ansItem} index={ansIndex} totalResult={item.length} type={item.type} /></li>
                         ))
                 }
             </div>
